7ee8278bf40b0a5d2fcf3f260bbabcd6151b4e74
Zagura.Vladimir <ZaguraVladimir@GMail.com>
2017-10-19 08:00:32 +0300

    Add 06

---
+++ src/l05/Main.kt
@@ -0,0 +1,12 @@
+package l05
+
+fun main(args: Array<String>) {
+    val st: String? = getSt()
+    val size = st?.length
+    println("st = ${st}, size = ${size}")
+}
+
+fun getSt(): String? {
+    //return "Hello"
+    return null
+}
---
+++ src/l06/Main.kt
@@ -0,0 +1,5 @@
+package l06
+
+fun main(args: Array<String>) {
+
+}
